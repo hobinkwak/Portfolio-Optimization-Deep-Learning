7ae04acf42703d63196b418ad86bfd5e11f27c1f
Hobin Kwak <76905712+hobinkwak@users.noreply.github.com>
2021-12-30 12:36:00 +0900

    Update gru.py

--- model/gru.py
+++ model/gru.py
@@ -4,19 +4,20 @@ import torch.nn.functional as F
 
 
 class GRU(nn.Module):
-    def __init__(self, n_layers, hidden_dim, n_stocks, dropout_p=0.5):
+    def __init__(self, n_layers, hidden_dim, n_stocks, dropout_p=0.3, bidirectional=False):
         super().__init__()
         self.n_layers = n_layers
         self.hidden_dim = hidden_dim
         self.gru = nn.GRU(
-            n_stocks, self.hidden_dim, num_layers=self.n_layers, batch_first=True
+            n_stocks, self.hidden_dim, num_layers=self.n_layers, batch_first=True, bidirectional=bidirectional
         )
         self.dropout = nn.Dropout(dropout_p)
-        self.fc = nn.Linear(self.hidden_dim, n_stocks)
+        self.scale = 2 if bidirectional else 1
+        self.fc = nn.Linear(self.hidden_dim * self.scale, n_stocks)
         self.swish = nn.SiLU()
 
     def forward(self, x):
-        init_h = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).to('cuda')
+        init_h = torch.zeros(self.n_layers * self.scale, x.size(0), self.hidden_dim).to('cuda')
         x, _ = self.gru(x, init_h)
         h_t = x[:, -1, :]
         logit = self.fc(self.dropout(h_t))
